77bb051cbf8131b6b39551e4b64e1c432165524a
Jefferson <jeffesoncarvalho10@gmail.com>
2022-07-13 19:40:15 -0300

    First Steps in Java with Spring Boot

---
+++ api-test/src/test/java/br/com/raspexemple/apitest/ApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raspexemple.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
